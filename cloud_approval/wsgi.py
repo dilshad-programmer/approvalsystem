0b0828aa734ec94c3c2b6493d6e37a306eb45032
dilshad-programmer <dilshadkannimuthu@gmail.com>
2026-04-15 07:33:40 +0000

    fix

--- cloud_approval/wsgi.py
+++ cloud_approval/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'approval_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud_approval.settings')
 
 application = get_wsgi_application()
